08dff0363f6d60d40121ec0e6b5a3bcec768f0b8
Gregory <elvismabisi@gmail.com>
2023-02-14 08:58:39 +0300

    completed lab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+    def match(array)
+        array.select do |letter|
+           letter.split("").sort == word.split("").sort #Checks to see if the sorted arrays of letters of each element of the input array and the "word" instance variable are equal 
+        end
+
+    end
+end
